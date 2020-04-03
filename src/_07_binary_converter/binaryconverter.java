a9756ed1e4ea00447d4a002de50c2d6fbf63648a
SimpleDoodle <48078201+SimpleDoodle@users.noreply.github.com>
2020-04-02 18:25:19 -0700

    :100:

--- src/_07_binary_converter/binaryconverter.java
+++ src/_07_binary_converter/binaryconverter.java
@@ -1,5 +1,37 @@
 package _07_binary_converter;
 
-public class binaryconverter {
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+public class binaryconverter {
+public static void main(String[] args) {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextField field = new JTextField();
+	frame.add(panel);
+	panel.add(field);
+	String convert; String input; {
+	    if(input.length() != 8){
+	        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+	        return "-";
+	    }
+	    String binary = "[0-1]+";//must contain numbers in the given range
+	    if (!input.matches(binary)) {
+	        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+	        return "-";   
+	    }
+	    try {
+	        int asciiValue = Integer.parseInt(input, 2);
+	        char theLetter = (char) asciiValue;
+	        return "" + theLetter;
+	    } catch (Exception e) {
+	        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+	        return "-";
+	    }
+	}
+
+
+}
 }
